25e13b3844e96485c979f960243c667153c70b06
Heshie Brody <4hbrody@gmail.com>
2020-01-02 20:40:26 -0500

    Add react router. Create user components folder. React router, Rails routes.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import HomeContainer from './home/HomeContainer.js';
+import { Route, Switch } from 'react-router-dom';
+
+import HomeContainer from './home/HomeContainer';
+import UserContainer from './user/UserContainer';
 
 class App extends React.Component {
   render () {
     return (
-      <HomeContainer />
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomeContainer}/>
+          <Route exact path='/users' component={UserContainer} />
+        </Switch>
+      </div>
     )
   }
 }
---
+++ app/javascript/components/user/UserContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import UserInfo from './components/UserInfo.js';
+
+export default class UserContainer extends Component{
+  render() {
+    return(
+      <UserInfo />
+    )
+  }
+}
