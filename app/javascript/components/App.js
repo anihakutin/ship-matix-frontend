b87c923d2128bf8cd6db37734a7ee74ad0689e8c
Heshie Brody <4hbrody@gmail.com>
2020-01-02 19:57:16 -0500

    Configure app root to load react top level page.

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomeContainer from './home/HomeContainer.js';
+
+class App extends React.Component {
+  render () {
+    return (
+      <HomeContainer />
+    )
+  }
+}
+
+export default App
---
+++ app/javascript/components/home/components/TitleBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class TitleBar extends Component{
+  render() {
+    return(
+      <h1>Not very fancy but you may consider me your shiny new home page!(for now...)</h1>
+      )
+  }
+}
