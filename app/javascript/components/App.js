612622296322a133d91992cdb528c20d8e5b3ab5
Heshie Brody <4hbrody@gmail.com>
2020-01-13 20:27:11 -0500

    Add react-router-dom imports. Add nav and links.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import HomeContainer from './home/HomeContainer';
 import UserContainer from './user/UserContainer';
@@ -9,6 +14,20 @@ class App extends React.Component {
   render () {
     return (
       <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/users/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/users">User Dashboard</Link>
+            </li>
+
+          </ul>
+        </nav>
         <Switch>
           <Route exact path="/" component={HomeContainer}/>
           <Route exact path='/users' component={UserContainer} />
