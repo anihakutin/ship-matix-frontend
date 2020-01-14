f66a5b3003aa6e505ca509824e2a9ba9d256b6b8
Heshie Brody <4hbrody@gmail.com>
2020-01-13 20:34:37 -0500

    Add connect, dispatch. Redirect to login on logged out.

--- app/javascript/components/user/components/UserInfo.js
+++ app/javascript/components/user/components/UserInfo.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { authedUser } from 'components/authActions.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
-export default class UserInfo extends Component{
-  render() {
-    return(
-      <h3>Hello User!</h3>
-      )
+class UserInfo extends Component{
+
+  componentDidMount() {
+    this.props.authedUser()
   }
+
+  render() {
+    const currentUser = this.props.user
+    if (!currentUser) {
+      return <Redirect to="/users/login"/>
+        }
+    //console.log(this.props.user)
+    return(
+    <div>
+      <h3>Hello {this.props.user.name}!</h3>
+    </div>
+    )
+    }
 }
+
+  const mapStateToProps = state => {
+    return {
+      user: state.auth.currentUser
+    }
+  }
+
+  const mapDispatch = dispatch => {
+    return {
+      authedUser: () => { dispatch(authedUser()) }
+    }
+  }
+
+
+export default connect(mapStateToProps, mapDispatch)(UserInfo)
