5efc1ba3391f7998a06d98f9d919c73e8d667410
Heshie Brody <4hbrody@gmail.com>
2020-01-13 20:28:01 -0500

    Add authentication actions for redux.

---
+++ app/javascript/components/authActions.js
@@ -0,0 +1,73 @@
+export function createUser(name, email, password) {
+  return (dispatch) => {
+    dispatch({ type: 'START_LOGIN_REQUEST' });
+    fetch('/api/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ user: { name, email, password } })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (!data.failure) {
+        localStorage.setItem("token", data.jwt);
+        return dispatch({ type: 'LOGIN_USER', user: data.user, success: data.success })
+      }
+      else {
+        return dispatch({ type: 'LOGIN_FAILED', error: data.failure })
+      }
+    })
+  }
+}
+
+export function loginUser(email, password) {
+  return (dispatch) => {
+    dispatch({ type: 'START_LOGIN_REQUEST' });
+    fetch('/api/login', {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+      })
+    .then(response => response.json())
+    .then(data => {
+      if (!data.failure) {
+        localStorage.setItem("token", data.jwt);
+        return dispatch({ type: 'LOGIN_USER', user: data.user, success: data.success })
+      }
+      else {
+        return dispatch({ type: 'LOGIN_FAILED', error: data.failure })
+      }
+    })
+  }
+}
+
+export function authedUser() {
+  const token = localStorage.getItem("token");
+  return (dispatch) => {
+    if (token) {
+      fetch(`/api/auto_login`, {
+       headers: {
+         Authorization: `Bearer ${token}`
+       }
+     })
+     .then(resp => resp.json())
+     .then(data => dispatch({ type: 'AUTHED_USER', user: data.user, success: data.success }))
+     .catch(() => dispatch({ type: 'LOGIN_FAILED' }))
+    }
+    else {
+      return dispatch({ type: 'LOGIN_FAILED', error: "User is not logged in" })
+    }
+  }
+}
+
+export function logoutUser() {
+  localStorage.removeItem("token")
+  return (dispatch) => {
+    return dispatch({ type: 'LOGOUT_USER', success: "User is logged out"})
+  }
+}
