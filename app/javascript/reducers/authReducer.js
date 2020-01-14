27bc93aacc522018060a9489e939ced5c3058671
Heshie Brody <4hbrody@gmail.com>
2020-01-13 20:23:47 -0500

    Add authentication reducer for redux.

---
+++ app/javascript/reducers/authReducer.js
@@ -0,0 +1,72 @@
+const authReducer =
+(state = {
+  currentUser: "",
+  loggedIn: false,
+  requesting: false,
+  error: "",
+  success: ""
+  }, action) => {
+  switch (action.type) {
+
+    case "START_LOGIN_REQUEST":
+      return {
+        ...state,
+        currentUser: state.currentUser,
+        requesting: true
+      }
+
+    case "LOGIN_FAILED":
+      return {
+        ...state,
+        currentUser: "",
+        requesting: false,
+        loggedIn: false,
+        error: action.error
+      }
+
+    case "LOGIN_USER":
+      return {
+        ...state,
+        currentUser: action.user,
+        requesting: false,
+        loggedIn: true,
+        error: "",
+        success: action.success
+      }
+
+    case "AUTHED_USER":
+      return {
+        ...state,
+        currentUser: action.user,
+        requesting: false,
+        loggedIn: true,
+        error: "",
+        success: action.success
+      }
+
+    case "CREATE_USER":
+      return {
+        ...state,
+        currentUser: action.user,
+        requesting: false,
+        loggedIn: true,
+        error: "",
+        success: action.success
+      }
+
+    case "LOGOUT_USER":
+    return {
+      ...state,
+      currentUser: "",
+      requesting: false,
+      loggedIn: false,
+      error: "",
+      success: action.success
+    }
+
+    default:
+      return state;
+  }
+}
+
+export default authReducer;
