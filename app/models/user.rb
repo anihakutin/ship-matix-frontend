ffc6dc812af6731b2fd8cb7a1ca1f96c276056fd
Heshie Brody <4hbrody@gmail.com>
2020-01-17 00:02:59 -0500

    Remove password validation when password doesn't exist.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :orders, :dependent => :delete_all
 
 
-  validates :name, :email, :password, presence: true
+  validates :name, :email, presence: true
   validates :email, uniqueness: true
-  validates :password, :length => {:within => 6..40}
+  validates :password, presence: true, :length => {:within => 6..40}, :if => :password
 end
