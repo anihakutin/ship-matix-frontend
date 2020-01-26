25891fcd944536ca30b1bfbe95de8793ae368497
Heshie Brody <4hbrody@gmail.com>
2020-01-26 15:46:23 -0500

    Add user info component.

--- app/javascript/components/user/components/UserInfo.js
+++ app/javascript/components/user/components/UserInfo.js
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { authedUser } from 'components/authActions.js';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation
-} from "react-router-dom";
 
-class UserInfo extends Component{
-
-  componentDidMount() {
-    this.props.authedUser()
-  }
+export default class UserInfo extends Component{
 
   render() {
-    const currentUser = this.props.user
-    if (!currentUser) {
-      return <Redirect to="/users/login"/>
-        }
-    //console.log(this.props.user)
     return(
-    <div>
-      <h3>Hello {this.props.user.name}!</h3>
-    </div>
-    )
+      <div>
+        <h3>Hello {this.props.user.name}!</h3>
+      </div>
+      )
     }
 }
-
-  const mapStateToProps = state => {
-    return {
-      user: state.auth.currentUser
-    }
-  }
-
-  const mapDispatch = dispatch => {
-    return {
-      authedUser: () => { dispatch(authedUser()) }
-    }
-  }
-
-
-export default connect(mapStateToProps, mapDispatch)(UserInfo)
