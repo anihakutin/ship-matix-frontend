254e31d4921775427eda63a3b177db16b22244bc
Heshie Brody <4hbrody@gmail.com>
2020-01-27 18:22:17 -0500

    Replace links with grommet components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Home, Notification, ChatOption } from "grommet-icons";
+import { Grommet, RoutedAnchor, Anchor, Header, Menu, Box, Nav, Button, Grid, Text, } from "grommet";
+import { grommet } from "grommet/themes";
+import { RouterAnchor } from "./user/components/links";
+// import { withStyles } from '@material-ui/core/styles';
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,26 +19,41 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/users/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/users">User Dashboard</Link>
-            </li>
+      <Grommet full theme={grommet}>
+      <Header background="brand">
+      <Box direction="row" align="center" gap="small">
+        <Anchor color="white" href="https://github.com/ShimiSun">
+          ShimiSun
+        </Anchor>
+      </Box>
+      <Nav direction="row">
+        <RouterAnchor icon={< Home />} path="/" hoverIndicator ></RouterAnchor>
+        <RouterAnchor icon={< Notification />} path="/users"  hoverIndicator ></RouterAnchor>
+        <RouterAnchor icon={< ChatOption />} path="/"  hoverIndicator ></RouterAnchor>
 
-          </ul>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={HomeContainer}/>
-          <Route exact path='/users' component={UserContainer} />
-          <Route exact path='/users/login' component={LoginContainer} />
-        </Switch>
+      </Nav>
+
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/users/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/users">User Dashboard</Link>
+              </li>
+
+            </ul>
+      </Header>
+      <Switch>
+        <Route exact path="/" component={HomeContainer}/>
+        <Route exact path='/users' component={UserContainer} />
+        <Route exact path='/users/login' component={LoginContainer} />
+      </Switch>
+      </Grommet>
       </div>
+
     )
   }
 }
