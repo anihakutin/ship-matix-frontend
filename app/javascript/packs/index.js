e350c2d51106c18fd15ec3db14e9d103d53141a1
Heshie Brody <4hbrody@gmail.com>
2020-01-27 18:22:38 -0500

    Import grommet

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { withStyles } from '@material-ui/core/styles';
-import { Grommet } from 'grommet';
 
 import { Provider } from 'react-redux';
 import store from '../reducers/index.js';
