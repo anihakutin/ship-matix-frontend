16bdad468c46c8db7767ce2ab50b60da24ba33fe
Heshie Brody <4hbrody@gmail.com>
2020-01-27 18:46:59 -0500

    Style header, align links.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Home, Notification, ChatOption } from "grommet-icons";
-import { Grommet, RoutedAnchor, Anchor, Header, Menu, Box, Nav, Button, Grid, Text, } from "grommet";
+import { Home, Notification, Dashboard, Login } from "grommet-icons";
+import { Grommet, RoutedAnchor, Anchor, Header, Menu, Box, Nav, Button, Grid, Text, Heading } from "grommet";
 import { grommet } from "grommet/themes";
 import { RouterAnchor } from "./user/components/links";
-// import { withStyles } from '@material-ui/core/styles';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -19,41 +19,21 @@ class App extends React.Component {
   render () {
     return (
       <div>
-      <Grommet full theme={grommet}>
-      <Header background="brand">
-      <Box direction="row" align="center" gap="small">
-        <Anchor color="white" href="https://github.com/ShimiSun">
-          ShimiSun
-        </Anchor>
-      </Box>
-      <Nav direction="row">
-        <RouterAnchor icon={< Home />} path="/" hoverIndicator ></RouterAnchor>
-        <RouterAnchor icon={< Notification />} path="/users"  hoverIndicator ></RouterAnchor>
-        <RouterAnchor icon={< ChatOption />} path="/"  hoverIndicator ></RouterAnchor>
-
-      </Nav>
-
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/users/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/users">User Dashboard</Link>
-              </li>
-
-            </ul>
-      </Header>
-      <Switch>
-        <Route exact path="/" component={HomeContainer}/>
-        <Route exact path='/users' component={UserContainer} />
-        <Route exact path='/users/login' component={LoginContainer} />
-      </Switch>
-      </Grommet>
+        <Grommet full theme={grommet}>
+          <Header background="brand" pad="xxsmall" align="end" justify="end">
+            <Nav direction="row" gap="small" alignSelf="end" >
+              <RouterAnchor icon={< Home />} path="/" hoverIndicator ></RouterAnchor>
+              <RouterAnchor icon={< Login />} path="/users/login"  hoverIndicator ></RouterAnchor>
+              <RouterAnchor icon={< Dashboard />} path="/users"  hoverIndicator ></RouterAnchor>
+            </Nav>
+          </Header>
+          <Switch>
+            <Route exact path="/" component={HomeContainer}/>
+            <Route exact path='/users' component={UserContainer} />
+            <Route exact path='/users/login' component={LoginContainer} />
+          </Switch>
+        </Grommet>
       </div>
-
     )
   }
 }
