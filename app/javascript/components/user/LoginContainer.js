e11d0e60f96fb9c49b80bc55fcd3a8c76aa1a08f
Heshie Brody <4hbrody@gmail.com>
2020-01-27 23:40:59 -0500

    Move messages from login component to container.

--- app/javascript/components/user/LoginContainer.js
+++ app/javascript/components/user/LoginContainer.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { Grommet, Box } from "grommet";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 import LoginForm from './components/LoginForm.js';
 import SignupForm from './components/SignupForm.js';
 
-export default class LoginContainer extends Component{
+class LoginContainer extends Component{
+  constructor() {
+    super()
+
+    this.state = {
+      message: ""
+    }
+  }
+
+  LoadingMsg = () => {
+    if (this.props.requesting) {
+      return "Logging In"
+      } else {
+        return ""
+      }
+  }
+  //Status: {this.LoadingMsg()}
   render() {
+    if (this.props.loggedIn) {
+      return <Redirect to="/users" />
+    }
+
     return(
-      <Box direction="column" pad="medium" align="center"  justify="start">
-        <LoginForm />
-        <SignupForm />
+      <Box direction="column" pad="medium" align="center" justify="center">
+        <p>
+          {this.props.messages.error || this.props.messages.success}
+        </p>
+        <Box direction="row" pad="medium" align="center"  justify="center" gap="large">
+          <LoginForm />
+          <SignupForm />
+        </Box>
       </Box>
       )
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    user: state.auth.currentUser,
+    messages: state.auth,
+    requesting: state.auth.requesting,
+    loggedIn: state.auth.loggedIn
+  }
+}
+
+export default connect(mapStateToProps)(LoginContainer)
