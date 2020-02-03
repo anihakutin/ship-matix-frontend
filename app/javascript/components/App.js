32e954f163304d551d1c1e9337b957ea5cfa6f6b
Heshie Brody <4hbrody@gmail.com>
2020-02-02 21:39:07 -0500

    Replace tags with Grommet components.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Home, Notification, Dashboard, Login } from "grommet-icons";
-import { Grommet, RoutedAnchor, Anchor, Header, Menu, Box, Nav, Button, Grid, Text, Heading } from "grommet";
+import { Grommet, Main, RoutedAnchor, Anchor, Header, Footer, Menu, Box, Nav, Button, Grid, Text, Heading } from "grommet";
 import { grommet } from "grommet/themes";
 import { RouterAnchor } from "./user/components/Links";
 import {
@@ -18,18 +18,24 @@ class App extends React.Component {
     return (
       <div>
         <Grommet full theme={grommet}>
-          <Header background="brand" pad="xxsmall" align="end" justify="end">
-            <Nav direction="row" gap="small" alignSelf="end" >
-              <RouterAnchor icon={< Home />} path="/" hoverIndicator ></RouterAnchor>
-              <RouterAnchor icon={< Login />} path="/users/login"  hoverIndicator ></RouterAnchor>
-              <RouterAnchor icon={< Dashboard />} path="/users"  hoverIndicator ></RouterAnchor>
-            </Nav>
-          </Header>
-          <Switch>
-            <Route exact path="/" component={HomeContainer}/>
-            <Route exact path='/users' component={UserContainer} />
-            <Route exact path='/users/login' component={LoginContainer} />
-          </Switch>
+          <Main full="true" background="light-2">
+            <Header background="neutral-3" pad="xxsmall" align="end" justify="end">
+              <Nav direction="row" gap="small" alignSelf="end" >
+                <RouterAnchor icon={< Home color="light-1" />} path="/" hoverIndicator ></RouterAnchor>
+                <RouterAnchor icon={< Login color="light-1" />} path="/users/login"  hoverIndicator ></RouterAnchor>
+                <RouterAnchor icon={< Dashboard color="accent-4" />} path="/users"  hoverIndicator ></RouterAnchor>
+              </Nav>
+            </Header>
+            <Switch>
+              <Route exact path="/" component={HomeContainer}/>
+              <Route exact path='/users' component={UserContainer} />
+              <Route exact path='/users/login' component={LoginContainer} />
+            </Switch>
+            <Footer background="neutral-3" pad="small" alignSelf="end" >
+              <Text>Copyright ShipMatix 2020</Text>
+              <Anchor label="About" color="accent-4" path="/about" />
+            </Footer>
+          </Main>
         </Grommet>
       </div>
     )
