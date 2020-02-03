444ceef973a174862ca8c4ee9deb4e2a23db63f1
Heshie Brody <4hbrody@gmail.com>
2020-02-02 23:37:55 -0500

    Remove map. Replace with icons.

--- app/javascript/components/home/components/Hero.js
+++ app/javascript/components/home/components/Hero.js
@@ -1,34 +1,29 @@
 import React, { Component } from 'react';
 import { Box, Text, Heading, Paragraph, WorldMap } from "grommet";
+import { Globe, Robot, Cubes, Next, Print } from "grommet-icons";
 
 export default class Hero extends Component{
   render() {
     return(
-      <Box alignSelf="center" direction="column" full="true" alignContent="stretch">
-        <Box direction="column">
-          <Heading level="2" pad="none" margin="xsmall">Go Global.</Heading>
-          <Heading level="4" pad="none" margin="xsmall">Automate your shipping. Expand Your Reach.</Heading>
+      <Box alignSelf="center" direction="column" alignContent="stretch" margin="small" pad="small" overflow="auto">
+        <Box direction="column" justify="center" alignContent="center" pad="small">
+          <Globe size="xlarge" />
+          <Heading level="1" pad="small" margin="small">Go Global.</Heading>
+          <Heading level="3" pad="none" margin="xsmall">Automate your shipping. Expand Your Reach.</Heading>
         </Box>
-        <Box alignSelf="center" alignContents="center" margin="small" pad="small">
-          <WorldMap
-            color="neutral-3"
-            continents={[
-              {
-                name: 'North America',
-                color: 'light-4'
-              },
-            ]}
-            onSelectPlace={(lat, lon) => {}}
-            places={[
-              {
-                name: 'NYC',
-                location: [40.7128, -74.0060],
-                color: 'accent-2',
-              },
-            ]}
-            selectColor="accent-2"
-          />
-        </Box>
+        <Box alignSelf="center" direction="row" alignContents="center" margin="small" pad="small">
+          <Box alignSelf = "center"
+          direction="row"
+          margin = "small"
+          pad = "medium" 
+          justify="start">
+            <Cubes color="light-1" size="xlarge" />
+            <Next color="light-1" size="large" />
+            <Robot color = "yellow" size = "xlarge" />
+            <Next color="light-1" size="large" />
+            <Print color="accent-1" size="xlarge" />
+          </Box>
+        </Box>        
       </Box>
       )
   }
