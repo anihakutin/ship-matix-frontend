2cf32f0042f8da0deb60ef00517553aad05f7388
Heshie Brody <4hbrody@gmail.com>
2020-02-02 23:37:32 -0500

    Restyle home page, colors.

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Home, Notification, Dashboard, Login } from "grommet-icons";
-import { Grommet, Main, RoutedAnchor, Anchor, Header, Footer, Menu, Box, Nav, Button, Grid, Text, Heading } from "grommet";
+import { Home, Notification, Dashboard, Login, UserSettings } from "grommet-icons";
+import { Grommet, Main, RoutedAnchor, Anchor, Header, Footer, Menu, Box, Nav, Button, Grid, Text, List, Heading} from "grommet";
 import { grommet } from "grommet/themes";
 import { RouterAnchor } from "./user/components/Links";
 import {
@@ -12,26 +12,29 @@ import {
 import HomeContainer from './home/HomeContainer';
 import UserContainer from './user/UserContainer';
 import LoginContainer from './user/LoginContainer';
+import OrderContainer from './order/OrderContainer';
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <Grommet full theme={grommet}>
-          <Main full="true" background="light-2">
-            <Header background="neutral-3" pad="xxsmall" align="end" justify="end">
+          <Main full="true" background="#15202B">
+            <Header pad="xxsmall" align="end" alignContent="end" justify="end" >
               <Nav direction="row" gap="small" alignSelf="end" >
-                <RouterAnchor icon={< Home color="light-1" />} path="/" hoverIndicator ></RouterAnchor>
-                <RouterAnchor icon={< Login color="light-1" />} path="/users/login"  hoverIndicator ></RouterAnchor>
-                <RouterAnchor icon={< Dashboard color="accent-4" />} path="/users"  hoverIndicator ></RouterAnchor>
+                <RouterAnchor icon={< Home color="light-1"  />} path="/" size="small" ></RouterAnchor>
+                <RouterAnchor icon={< Login color="accent-4" />} path="/users/login" size="small" ></RouterAnchor>
+                <RouterAnchor icon={< UserSettings color="accent-4" />} path="/users" size="small" ></RouterAnchor>
+                <RouterAnchor icon={< Dashboard color="accent-4" />} path="/users/orders" size="small" ></RouterAnchor>
               </Nav>
             </Header>
             <Switch>
               <Route exact path="/" component={HomeContainer}/>
               <Route exact path='/users' component={UserContainer} />
               <Route exact path='/users/login' component={LoginContainer} />
+              <Route exact path='/users/orders' component={OrderContainer} />
             </Switch>
-            <Footer background="neutral-3" pad="small" alignSelf="end" >
+            <Footer pad="small" alignSelf="end" >
               <Text>Copyright ShipMatix 2020</Text>
               <Anchor label="About" color="accent-4" path="/about" />
             </Footer>
